42ff030a9c6cec0829790314ec948014be7bbb9d
RehamanMohammad <rehamanmohammad123@gmail.com>
2026-08-25 21:03:31 +0530

    Initial commit

---
+++ deliverymanagement/src/main/java/com/dm/deliverymanagement/DeliverymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.dm.deliverymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymanagementApplication.class, args);
+	}
+
+}
