eed195b7a9622ddd7e9793e2bbfd63bd0bfca5a8
RehamanMohammad <rehamanmohammad123@gmail.com>
2026-08-25 20:27:04 +0530

    Adding RestaurantManagement project

---
+++ restaurantmangement/src/test/java/com/rm/restaurantmangement/RestaurantmangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.restaurantmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantmangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
