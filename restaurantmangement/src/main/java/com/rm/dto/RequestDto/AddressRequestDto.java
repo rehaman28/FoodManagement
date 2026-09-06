aaf7fd8ec819283a1d5bff0805342161650cd24e
Rehaman_28 <67013179+rehaman28@users.noreply.github.com>
2026-09-06 18:12:17 +0530

    add address validation for restaurant requests

--- restaurantmangement/src/main/java/com/rm/dto/RequestDto/AddressRequestDto.java
+++ restaurantmangement/src/main/java/com/rm/dto/RequestDto/AddressRequestDto.java
@@ -1,5 +1,8 @@
 package com.rm.dto.RequestDto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressRequestDto {
+
     private String landmark;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String city;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String state;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{6}$", message = "Pincode must contain 6 digits")
     private String pincode;
 }
